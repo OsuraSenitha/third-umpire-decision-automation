d9af2ae4a0d021326de1350e21055bd4d55c2ec8
Osura Senitha <osura.2018854@iit.ac.lk>
2023-04-15 09:48:42 +0000

    Build and deploy permission fixed

--- backend/build-and-push.bash
+++ backend/build-and-push.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 870481100706.dkr.ecr.ap-southeast-1.amazonaws.com
-docker build -t third-umpire-decision-automation .
-docker tag third-umpire-decision-automation 870481100706.dkr.ecr.ap-southeast-1.amazonaws.com/third-umpire-decision-automation:latest
-docker push 870481100706.dkr.ecr.ap-southeast-1.amazonaws.com/third-umpire-decision-automation:latest
+aws ecr get-login-password --region ap-south-1 | sudo docker login --username AWS --password-stdin 870481100706.dkr.ecr.ap-south-1.amazonaws.com
+sudo docker build -t third-umpire-decision-automation .
+sudo docker tag third-umpire-decision-automation 870481100706.dkr.ecr.ap-south-1.amazonaws.com/third-umpire-decision-automation:latest
+sudo docker push 870481100706.dkr.ecr.ap-south-1.amazonaws.com/third-umpire-decision-automation:latest
