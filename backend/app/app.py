f81cd7d73b4b37a6e68e27bf9262d4dddb6badae
Osura Senitha <osura.2018854@iit.ac.lk>
2023-04-15 10:13:36 +0000

    Return value bug fix

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,8 +1,8 @@
 def handler(event, context):
     print(event)
     annotations = [
-        [10,20,30,40],
-        [50,60,70,80]
+        [1,10,20,30,40],
+        [2,50,60,70,80]
     ]
     print(f"Sending annotations: {annotations}")
     
@@ -12,6 +12,6 @@ def handler(event, context):
             "Content-Type": "application/json"
         },
         "body": {
-            "annotations ": annotations
+            "annotations": annotations
         }
     }
