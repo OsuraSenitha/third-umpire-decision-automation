c63d61c89aceb519b2535283e9a93b0fa0661869
OsuraSenitha <osura.2018854@iit.ac.lk>
2023-04-16 18:45:58 +0530

    method update

--- backend/experiments/src/data/process.py
+++ backend/experiments/src/data/process.py
@@ -1,11 +1,14 @@
 import numpy as np
 import cv2 as cv
 from .analyze import img2ColorMat
+import os
+import shutil
 
 def getBoundingBoxesFromSegmentation(seg_img, labels):
     colorMat = img2ColorMat(seg_img)
-    box_groups = []
-    for label in labels:
+    img_h, img_w, _ = seg_img.shape
+    annotations = []
+    for label_i, label in enumerate(labels):
         color = label["color"]
         thresh = np.isin(colorMat, color).astype(np.uint8)
         contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
@@ -16,9 +19,34 @@ def getBoundingBoxesFromSegmentation(seg_img, labels):
             for cont, no_par in zip(contours, no_parent):
                 if no_par:
                     box = cv.boundingRect(cont)
-                    bounding_rects.append(box)
-                    # x,y,w,h = box
+                    x,y,w,h = box
+                    x_c, y_c = (x+w/2)/img_w, (y+h/2)/img_h
+                    w_c, h_c = w/img_w, h/img_h
+                    bounding_rects.append([label_i, x_c, y_c, w_c, h_c])
                     # new_img = cv.rectangle(seg_img,(x,y),(x+w,y+h),(0,255,0),2)
-            box_groups.append({"name": label["name"], "boxes": bounding_rects})
+            annotations.extend(bounding_rects)
 
-    return box_groups
+    return annotations
+
+def splitForYOLO(yolo_dataset_path, data_path, train_weight, val_weight):
+    subdirs = ["images", "labels"]
+    splits = ["train", "val"]
+
+    for subdir in subdirs:
+        for split in splits:
+            path = f"{yolo_dataset_path}/{subdir}/{split}"
+            if not os.path.exists(path):
+                os.makedirs(path)
+
+    lbls_src_dir = f"{data_path}/annotations"
+    imgs_src_dir = f"{data_path}/images"
+    lbl_names = os.listdir(lbls_src_dir)
+    tot_weight = train_weight + val_weight
+    for i, lbl_name in enumerate(lbl_names):
+        dst_split = "val"
+        if (i%tot_weight)-train_weight < 0:
+            dst_split = "train"
+        img_name = os.path.splitext(lbl_name)[0] + ".png"
+        shutil.copy(f"{lbls_src_dir}/{lbl_name}", f"{yolo_dataset_path}/labels/{dst_split}")
+        shutil.copy(f"{imgs_src_dir}/{img_name}", f"{yolo_dataset_path}/images/{dst_split}")
+        
