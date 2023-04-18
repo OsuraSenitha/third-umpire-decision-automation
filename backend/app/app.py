ebfe74a2a353ecffd6f98e512c52cf8a8518a79f
OsuraSenitha <osura.2018854@iit.ac.lk>
2023-04-18 18:27:51 +0530

    added model codes

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,9 +1,26 @@
+from ultralytics import YOLO
+import torch
+import boto3
+import os
+
+model = YOLO('yolov8n.pt')
+s3_client = boto3.client("s3")
+BUCKET_NAME = "third-umpire-decision-automation-osura"
+
 def handler(event, context):
     print(event)
-    annotations = [
-        [1,10,20,30,40],
-        [2,50,60,70,80]
-    ]
+
+    audio_key = event["audioKey"]
+    filename = os.path.basename(audio_key)
+    download_path = f"/tmp/{filename}"
+
+    s3_client.download_file(Bucket=BUCKET_NAME, Key=audio_key, Filename=download_path)
+    pred = model.predict(download_path)[0]
+    cls = pred.cls
+    xywh = pred.xywh
+    cls_col = cls.reshape((-1, 1))
+    annotations = torch.cat((xywh, cls_col), dim=1).tolist()
+
     print(f"Sending annotations: {annotations}")
     
     return {
