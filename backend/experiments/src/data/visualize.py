191185d278af733a0ede783d677a329e23a9b1c2
OsuraSenitha <osura.2018854@iit.ac.lk>
2023-04-22 09:45:09 +0530

    visualize segments

--- backend/experiments/src/data/visualize.py
+++ backend/experiments/src/data/visualize.py
@@ -1,5 +1,7 @@
 import cv2 as cv
 from .process import cvtAnnotationsTXT2LST
+from typing import Tuple
+import numpy as np
 
 def drawRects(img, txt_cntnt, xywh=True, scaled=False, color=(0,255,0)):
     lines = txt_cntnt
@@ -20,5 +22,26 @@ def drawRects(img, txt_cntnt, xywh=True, scaled=False, color=(0,255,0)):
             xmax, ymax = int(x+w/2),int(y+h/2)
 
         drawn_img = cv.rectangle(drawn_img, (xmin, ymin), (xmax, ymax), color, int(min(H, W)/100))
+
+    return drawn_img
+
+def drawSegment(
+        img:np.ndarray, pts:Tuple[float], normalized:bool=True, color:Tuple[int]=[0,255,0], overlay_ratio:float=0.3
+    ) -> np.ndarray:
+    H, W, _ = img.shape
+    X = np.array(pts[0::2])
+    Y = np.array(pts[1::2])
+    if normalized:
+        X = (X*W).astype(int)
+        Y = (Y*H).astype(int)
+    
+    conts = (np.stack((X, Y)).T)[np.newaxis, :]
+    drawn_img = cv.drawContours(img.copy(), conts, -1, color)
+    overlay_img = cv.fillPoly(img.copy(), pts=conts, color=color)
+    drawn_img = (drawn_img*(1-overlay_ratio)+overlay_img*overlay_ratio).astype(drawn_img.dtype)
+
+    point_radius = max(int(min(H, W)/160), 1)
+    for x, y in zip(X, Y):
+      drawn_img = cv.circle(drawn_img, center=(x, y), radius=point_radius, color=color, thickness=-1)
 
     return drawn_img
