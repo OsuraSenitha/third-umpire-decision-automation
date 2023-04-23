9178e2e295ea15e2b6ab420e4a418648e784ed13
OsuraSenitha <osura.2018854@iit.ac.lk>
2023-04-23 09:54:40 +0530

    splitting method updated

--- backend/experiments/src/data/process.py
+++ backend/experiments/src/data/process.py
@@ -7,44 +7,31 @@ import json
 from tqdm.auto import tqdm
 from typing import Tuple
     
-def splitForObjectDetect(src_dataset_path, train_weight, val_weight, dst_dataset_path):
-    subdirs = ["bboxes", "images", "segmentations"]
+def splitForObjectDetect(src_dataset_dir, train_weight, val_weight, dst_dataset_dir, subdirs = ["bboxes", "images", "segmentations"], subdir_exts=["txt", "png", "png"]):
+    
     splits = ["train", "val"]
 
     for subdir in subdirs:
         for split in splits:
-            path = f"{dst_dataset_path}/{subdir}/{split}"
+            path = f"{dst_dataset_dir}/{subdir}/{split}"
             if not os.path.exists(path):
                 os.makedirs(path)
 
-    src_bbxs_dir = f"{src_dataset_path}/{subdirs[0]}"
-    src_imgs_dir = f"{src_dataset_path}/{subdirs[1]}"
-    src_segs_dir = f"{src_dataset_path}/{subdirs[2]}"
+    src_dirs = list(map(lambda subdir: f"{src_dataset_dir}/{subdir}", subdirs))
+    dst_dirs = list(map(lambda subdir: f"{dst_dataset_dir}/{subdir}", subdirs))
 
-    dst_bbxs_dir = f"{dst_dataset_path}/{subdirs[0]}"
-    dst_imgs_dir = f"{dst_dataset_path}/{subdirs[1]}"
-    dst_segs_dir = f"{dst_dataset_path}/{subdirs[2]}"
-
-    lbl_names = os.listdir(src_bbxs_dir)
+    obj_names = os.listdir(src_dirs[0])
     tot_weight = train_weight + val_weight
-    for i, lbl_name in enumerate(lbl_names):
+    for i, obj_name in enumerate(obj_names):
+        obj_name = os.path.splitext(obj_name)[0]
         dst_split = "val"
         if (i%tot_weight)-train_weight < 0:
             dst_split = "train"
-        img_name = os.path.splitext(lbl_name)[0] + ".png"
-        src_bbx_path = f"{src_bbxs_dir}/{lbl_name}"
-        src_img_path = f"{src_imgs_dir}/{img_name}"
-        src_seg_path = f"{src_segs_dir}/{img_name}"
-        dst_bbx_path = f"{dst_bbxs_dir}/{dst_split}"
-        dst_img_path = f"{dst_imgs_dir}/{dst_split}"
-        dst_seg_path = f"{dst_segs_dir}/{dst_split}"
 
-        shutil.copy(src_bbx_path, dst_bbx_path)
-        shutil.copy(src_img_path, dst_img_path)
-        if os.path.exists(src_seg_path): shutil.copy(src_seg_path, dst_seg_path)
-
-    if len(os.listdir(dst_segs_dir)) == 0:
-        shutil.rmtree(dst_segs_dir)
+        for src_dir, dst_dir, ext in zip(src_dirs, dst_dirs, subdir_exts):
+            src_path = f"{src_dir}/{obj_name}.{ext}"
+            dst_dir = f"{dst_dir}/{dst_split}"
+            shutil.copy(src_path, dst_dir)
 
 def cvtAnnotationsTXT2LST(txt_cntnt):
     lst = list(map(lambda line: [int(line.split()[0]), *list(map(float, line.split()[1:]))], txt_cntnt.strip().split("\n")))
