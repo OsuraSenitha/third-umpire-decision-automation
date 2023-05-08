0d034b8d68d94a14acdae09a912d272af39806e3
OsuraSenitha <osura.2018854@iit.ac.lk>
2023-05-09 04:04:06 +0530

    sagemaker training scripts

---
+++ backend/experiments/sandboxes/sagemaker-scripts-segmentation/main.py
@@ -0,0 +1,35 @@
+import yaml
+from ultralytics import YOLO
+import os
+
+if __name__ == "__main__":
+    data_dir = os.environ["SM_CHANNEL_TRAIN"]
+    model_output_dir = os.environ["SM_MODEL_DIR"]
+    intermediate_data_dir = os.environ["SM_OUTPUT_INTERMEDIATE_DIR"]
+    output_data_dir = os.environ["SM_OUTPUT_DATA_DIR"]
+
+    # read the data configuration and create the yaml file
+
+    dataset_config = {
+        "train": f"{data_dir}/images/train",
+        "val": f"{data_dir}/images/val",
+        "names": ["Batsman"],
+        "nc": 1,
+    }
+    ds_config_path = f"{data_dir}/dataset.yaml"
+    with open(ds_config_path, "w") as handler:
+        yaml.dump(dataset_config, handler)
+
+    patience = 50
+    epochs = 400
+
+    model = YOLO("yolov8n-seg.pt")
+    model.train(
+        data=ds_config_path,
+        epochs=epochs,
+        patience=patience,
+        pretrained=True,
+        project=output_data_dir,
+        name="batsman-segmentation",
+        optimizer="Adam",
+    )
